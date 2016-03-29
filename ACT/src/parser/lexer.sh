b5623645df9696242c86e4f659b652b0d0f85a47
sanjeev <sanjeev.kumar@iiitb.org>
2016-03-29 15:12:25 +0530

    Updated the build.xml and the lexer.sh

--- ACT/src/parser/lexer.sh
+++ ACT/src/parser/lexer.sh
@@ -2,7 +2,7 @@
 
 base_dir="/home/sanjeev/workspace_mars/ACT"
 
-java -jar $base_dir/JLex.jar $base_dir/src/parser/ACT3.lex
+java -jar ./JLex.jar ./src/parser/ACT3.lex
 
-mv $base_dir/src/parser/ACT3.lex.java $base_dir/src/parser/Yylex.java 
+mv ./src/parser/ACT3.lex.java ./src/parser/Yylex.java 
 
